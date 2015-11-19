3edc840303cb23db0429625232cd112e53facced
gillesw <gillesw@gmail.com>
2015-11-19 23:54:37 +0100

    Chunk 4096

--- py-dlchunk.py
+++ py-dlchunk.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 import os.path
+import os
 
 r=requests.get("https://github.com/braoru/Kibana/archive/master.zip",stream=True)
 
@@ -10,9 +11,8 @@ pprint(int(r.headers['Content-Length']))
 i=1
 
 with open('master.zip',mode='wb') as f:
-	for chunk in r.iter_content(chunk_size=1024):
+	for chunk in r.iter_content(chunk_size=4096):
 		f.write(chunk)
-		f.flush
 		pprint(i)
 		i=i+1
 		pprint(os.path.getsize("master.zip"))
