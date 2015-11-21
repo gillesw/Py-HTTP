fe60429d3b7b57f0a687841bcac15d6cbd8bc330
gillesw <gillesw@gmail.com>
2015-11-21 14:09:46 +0100

    Test sur les regexp

--- py-dlchunk.py
+++ py-dlchunk.py
@@ -2,19 +2,26 @@ import requests
 from pprint import pprint
 import os.path
 import os
+import re
 
-r=requests.get("https://github.com/braoru/Kibana/archive/master.zip",
+r=requests.get("http://localhost/i.jpg",
 	headers={'Range': 'bytes=4000-8000'}, stream=True)
 
 size=int(r.headers['Content-Length'])
 
+test=r.headers['Content-Range']
+
+match=re.match('^b',test).span
+
 pprint(size)
+pprint(test)
+pprint(match)
 
 i=1
 
-with open('master.zip',mode='wb') as f:
+with open('i.jpg',mode='wb') as f:
 	for chunk in r.iter_content(chunk_size=4096):
 		f.write(chunk)
 		pprint(i)
 		i=i+1
-		pprint(os.path.getsize("master.zip"))
+		pprint(os.path.getsize("i.jpg"))
