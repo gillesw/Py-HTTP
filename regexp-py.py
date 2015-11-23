d863491fceeff0f2b25dc14e1886ef1229b7b72a
gillesw <gillesw@gmail.com>
2015-11-23 20:51:27 +0100

    Test

--- regexp-py.py
+++ regexp-py.py
@@ -1,13 +1,18 @@
 import re
 
-#istring='bytes 4000-8000/3022098'
+istring='bytes 4000-8000/3022098'
 
-istring='bytes 4'
+#istring='bytes 4'
 
-pattern='^bytes'
+pattern='([0-9]+)-([0-9]+)/([0-9]+)'
 
 comp=re.compile(pattern)
 
 result=comp.search(istring)
 
-print(result.group)
+#print(result)
+
+if result==None:
+	print("Mismatch")
+else :
+	print(result.group(3))
